4fa31474f26e2467140b9cbf94c5442cd420f9dd
jlagarespo <jlagarespo@iebesalu.cat>
2019-01-01 14:25:58 +0100

    added a gitignore

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ nasm -f bin krnl/main.asm -o bin/kernelos.exe
 
 # Build
 echo ">> Building..."
-cat bin/bootsect.exe bin/kernelos.exe > unreal.img
+cat bin/bootsect.exe bin/kernelos.exe > bin/unreal.img
 
 # Clean
 echo ">> Cleaning..."
 cd bin
-rm * -rf
+rm *.exe *.bin *.o -rf
 cd ..
 
 # Test
 echo ">> Starting QEMU..."
-qemu-system-i386 -fda unreal.img -d guest_errors
+qemu-system-i386 -fda bin/unreal.img -d guest_errors
