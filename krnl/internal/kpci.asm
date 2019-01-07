8867a97a95d1c0017cc44c3e878e627f9fd10dd2
jlagarespo <jlagarespo@iebesalu.cat>
2019-01-07 12:37:06 +0100

    improve kpci

--- krnl/internal/kpci.asm
+++ krnl/internal/kpci.asm
@@ -5,26 +5,40 @@
 ; an error while shutting down, there is no
 ; going back.
 system_powerdown:
+    ; No need to comment this first
+    ; label of the subroutine, as it is mostly
+    ; grahpical support for the shutdown
+    ; (this way it is less boring for the user)
+
+inform_user_shutdown_request_send:
+    ; Hide cursor
     mov ah, 0x01
     mov cx, 0x2007
     int 0x10
 
+    ; Ugly formatted printing
     call print_endl
     mov si, str_ok
     call teleprint
-
     call print_endl
     mov si, str_stage_1
     call teleprint
+
+request_bios_poweroff:
     ; Request global power off
+    ; to the BIOS
     mov ah, 0x42
     mov al, 0x00
     int 0x15
 
+shutdown_animation_wait_loop:
+    ; And give it some time
+    ; with a cool animation
     call print_endl
     mov si, str_stage_2
     call teleprint
 
+    ; Some cool animation
     mov al, ']'
     mov ah, 0x09
     mov cx, 0x01
@@ -34,7 +48,11 @@ system_powerdown:
     ; Wait some time
     ; (with a cool animation ;)
     mov cx, 0x05
-wait_loop:
+shutdown_wait_loop:
+    ; Again graphical
+    ; "progress bar"
+    ; as we wait for the bios, in case
+    ; something went wrong
     mov ah, 0x86
     xor dx, dx
     int 0x15
@@ -48,21 +66,31 @@ wait_loop:
     mov bl, 0x02
     int 0x10
     pop cx
-    loop wait_loop
+    loop shutdown_wait_loop
 
+    ; Restore cursor
     mov ah, 0x01
     mov cx, 0x0007
     int 0x10
 
-    ; Halt CPU
+    ; Print final things
     call print_endl
     mov si, str_stage_3
     call teleprint
+
+    ; GRAHICAL SHUTDOWN ENDS HERE. STARTING ACTUAL SHUTDOWN ROUTINE
+power_off_computer_via_bios:
+    ; We won't need to print anything again,
+    ; so disable interrupts
+    cli
+
     mov ah, 0x53
     xor bx, bx
     int 0x15
 
     ; Power off
+    ; let the bios do the hard work for us,
+    ; as we aren't in unreal mode for any reason...
     mov ax, 0x530e
     xor bx, bx
     mov cx, 0x0102
@@ -74,14 +102,22 @@ wait_loop:
     int 0x15
 
 system_powerdown_halt:
+    ; Ok, it failed...
+    ; Reenable interrupts to let the use know
+    ; that the shutdown failed (we need interrupts
+    ; to print stuff)
+    sti
+
+    ; The actual ugly formatted prints
     call print_endl
     mov si, str_error_s
     call teleprint
     call print_endl
     mov si, str_error_d
     call teleprint
-    mov cx, 0x03
 
+    ; Halt the computer forever
+    ; until use manually hard-powers-off the computer.
     jmp system_halt
 
 system_reboot:
@@ -98,6 +134,15 @@ system_reboot:
 system_halt:
     nop
 halt_loop:
+    ; Halt until next interrupt
+    ; having interrupts disabled...
+    ; this sounds to me like an
+    ; infinite loop......
     cli
     hlt
+    ; If for any weird reason, we get here(we shouldn't
+    ; because interrupts are disabled and they can't
+    ; call to continue execution), whatever,
+    ; if we get here for any reason,
+    ; loop back again
     jmp halt_loop
